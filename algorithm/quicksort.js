31669319d5024f9631cfe2a5e00261240bb77c32
Young-Mason <mason.yk3@gmail.com>
2021-02-23 17:58:25 +0900

    [Algorithm] Updated quickSort

--- algorithm/quicksort.js
+++ algorithm/quicksort.js
@@ -1,4 +1,22 @@
+//Code states Naive solution
+const quickSort = function (arr) {
+  if (arr.length <= 1) return arr;
 
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  const lSorted = quickSort(left);
+  const rSorted = quickSort(right);
+  return [...lSorted, pivot, ...rSorted];
+};
+
+// Udemy lecture solution
 function pivot(arr, start = 0, end = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
@@ -20,22 +38,18 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIdx;
 }
 
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
-function quickSort(arr, left = 0, right = arr.length -1){
-    if(left < right){
-        let pivotIndex = pivot(arr, left, right) //3
-        //left
-        quickSort(arr,left,pivotIndex-1);
-        //right
-        quickSort(arr,pivotIndex+1,right);
-      }
-     return arr;
-} 
-           
-quickSort([100,-3,2,4,6,9,1,2,5,3,23])
-
-
-
+quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]);
 
 // [4,6,9,1,2,5,3]
 // [3,2,1,4,6,9,5]
@@ -45,7 +59,3 @@ quickSort([100,-3,2,4,6,9,1,2,5,3,23])
 //  2,1      5  9
 //    2
 //  1
-
-
-
-
