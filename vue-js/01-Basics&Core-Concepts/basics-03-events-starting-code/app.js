0db275094c058083628dbb4e23ce709f38244027
Young-Mason <mason.yk3@gmail.com>
2021-04-03 23:24:47 +0900

    [VueJS] #01-Basics&Core Update event-binding

--- vue-js/01-Basics&Core-Concepts/basics-03-events-starting-code/app.js
+++ vue-js/01-Basics&Core-Concepts/basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    addCount(num = 1) {
+      this.counter = this.counter + num;
+    },
+    removeCount(num = 1) {
+      this.counter = this.counter - num;
+    },
+    setName(e, lastName) {
+      this.name = e.target.value + " " + lastName;
+    },
+    submitForm() {
+      alert("submitted");
+    },
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
